d455097c1d2ff4fa424eb466abccd9cb9189f17b
Hong Son <dohongson27@gmail.com>
2024-04-16 18:25:21 +0700

    add file

---
+++ hienThiDauRa.js
@@ -0,0 +1,12 @@
+function change() {
+    alert('Thuy xinh dep nhat the gioi');
+}
+function change1() {
+    document.getElementById('hienThi').innerHTML = 'xin chao';
+}
+function change2() {
+    document.write('hihi');
+}
+function change3() {
+    console.log('test');
+}
